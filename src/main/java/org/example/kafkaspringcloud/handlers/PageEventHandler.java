aedcb0dcfc6874e1f4481c96bfc30ec2fe6c1b06
zakaria-el-guazzar <elguazzarzakaria10@gmail.com>
2025-10-12 23:10:34 +0100

    Add the page index.html and some modification in PageEventController and PageEventHandler

--- src/main/java/org/example/kafkaspringcloud/handlers/PageEventHandler.java
+++ src/main/java/org/example/kafkaspringcloud/handlers/PageEventHandler.java
@@ -42,7 +42,7 @@ public class PageEventHandler {
                 input.filter((k,v)->v.duration()>100)
                 .map((k,v)->new KeyValue<>(v.name(),v.duration()))
                         .groupByKey(Grouped.with(Serdes.String(),Serdes.Long()))
-                        .windowedBy(TimeWindows.of(Duration.ofSeconds(5000)))
+                        .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
                         .count(Materialized.as("count-store"))
                         .toStream()
                         .map((k,v)->new KeyValue<>(k.key(),v));
